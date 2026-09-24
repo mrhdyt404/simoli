e2a81089f55513e98cb29cb13e75b576ca276ef4
mrhdyt404 <124226649+mrhdyt404@users.noreply.github.com>
2026-09-24 13:01:18 +0700

    Delete simoli_monitoring_mobile directory
    tidak di pakai

--- simoli_monitoring_mobile/test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:simoli_monitoring_mobile/main.dart';
-
-void main() {
-  testWidgets('Operator App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const SimoliMonitoringApp());
-    expect(find.text('SIMOLI OPERATOR LAPANGAN'), findsOneWidget);
-  });
-}
